9cdbac5a279b1b78128d9b1276e5adeb0f167270
reignjosephdelossantos <202211361@btech.ph.education>
2025-03-04 06:49:02 +0000

    wa

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
 document.getElementById("loginBtn").addEventListener("click", function() {
     this.classList.add("clicked");
 });
+
+function togglePassword() {
+    let passwordField = document.getElementById("password");
+    let hideIcon = document.getElementById("hideIcon");
+    let showIcon = document.getElementById("showIcon");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";   
+        hideIcon.style.display = "none";
+        showIcon.style.display = "inline"; 
+    } else {
+        passwordField.type = "password"; 
+        hideIcon.style.display = "inline";
+        showIcon.style.display = "none"; 
+    }
+}
